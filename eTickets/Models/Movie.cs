0c2793ed1cae39699aff3349c86ed4f56031b9c2
Edvard <eddeheijer@hotmail.com>
2021-08-16 14:13:32 +0200

    Added all controllers and all views

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,47 @@
+﻿using eTickets.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+
+        //Relationships
+
+        public List<Actor_Movie> Actors_Movies { get; set; }
+
+        //Cinema
+        public int CinemaId { get; set; }
+
+        [ForeignKey("CinemaId")] //You dont have to do this
+        public Cinema Cinema { get; set; }
+
+        //Producers
+        public int ProducerId { get; set; }
+
+        [ForeignKey("ProducerId")] //You dont have to do this
+        public Producer Producer { get; set; }
+
+    }
+}
